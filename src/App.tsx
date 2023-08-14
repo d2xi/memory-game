a35d04fa9fb026ab7b41204e3c5f7bcb79fb70fa
d2xi <90256198+d2xi@users.noreply.github.com>
2023-08-15 01:52:48 +0200

    Implement scalable layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import Card from "./components/Card";
 
+const shuffleArray = (array: Array<string>) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
 function App() {
-  const cards = Array(24).fill("X");
+  const cards = Array(10).fill("|").concat(Array(10).fill("="));
+  shuffleArray(cards);
+  const click = () => console.log("clicked");
   return (
     <div className="grid">
       {cards.map((card) => (
-        <Card cardLabel={card} />
+        <Card cardLabel={card} onClick={click} />
       ))}
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,11 @@
 interface CardProps {
   cardLabel: string;
+  onClick: () => void;
 }
-export default function Card({ cardLabel }: CardProps) {
-  return <div className="card">{cardLabel}</div>;
+export default function Card({ cardLabel, onClick }: CardProps) {
+  return (
+    <div className="card" onClick={onClick}>
+      {cardLabel}
+    </div>
+  );
 }
