1da518c5e150b53187f471f6482a17be4c6fe88c
d2xi <90256198+d2xi@users.noreply.github.com>
2023-08-15 15:31:29 +0200

    Implementing revealing and hiding functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import Card from "./components/Card";
+import Card, { ICard } from "./components/Card";
 
 const shuffleArray = (array: Array<string>) => {
   for (let i = array.length - 1; i > 0; i--) {
@@ -10,14 +11,38 @@ const shuffleArray = (array: Array<string>) => {
   }
 };
 
-function App() {
-  const cards = Array(10).fill("|").concat(Array(10).fill("="));
+function initBoard(): string[] {
+  // const cards = "abcdefghijklmnqrstuvwxyz".repeat(2).split("");
+  const cards = "ab".repeat(2).split("");
   shuffleArray(cards);
-  // const click = () => console.log("clicked");
+  return cards;
+}
+
+function App() {
+  const [cards] = useState(initBoard());
+  const [firstChoice, setFirstChoice] = useState<ICard>();
+
+  const handleClick = (card: ICard) => {
+    console.log(firstChoice);
+    if (firstChoice === undefined) {
+      console.log("set");
+      setFirstChoice(card);
+    } else if (firstChoice.getLabel() === card.getLabel()) {
+      console.log(firstChoice.getLabel + " match " + card.getLabel);
+      setFirstChoice(undefined); // do not like the usage of "undefined" -> check Optionals or something
+    } else {
+      console.log(
+        firstChoice.getLabel() + " does not match " + card.getLabel()
+      );
+      firstChoice.hide();
+      card.hide();
+      setFirstChoice(undefined);
+    }
+  };
   return (
     <div className="grid">
-      {cards.map((card) => (
-        <Card cardLabel={card} />
+      {cards.map((card, index) => (
+        <Card key={index} cardLabel={card} onClick={handleClick} />
       ))}
     </div>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 
-interface CardProps {
+export interface CardProps {
   cardLabel: string;
-  // onClick: () => void;
+  onClick: (card: ICard) => void;
 }
-export default function Card({ cardLabel }: CardProps) {
-  const [isSelected, setState] = useState(false);
+export interface ICard {
+  getLabel: () => string;
+  hide: () => void;
+}
+export default function Card({ cardLabel, onClick }: CardProps) {
+  const [isReviled, setIsReviled] = useState(false);
+  const theCard: ICard = {
+    hide: () => {
+      setIsReviled(false);
+      console.log("the card " + cardLabel + " was hidden");
+    },
+    getLabel: () => cardLabel,
+  };
+  const handleClick = () => {
+    if (!isReviled) {
+      setIsReviled(true);
+      console.log("the card " + cardLabel + " was reviled");
+      onClick(theCard);
+    }
+  };
   return (
-    <div className="card" onClick={() => setState(!isSelected)}>
-      {isSelected && cardLabel}
+    <div className="card" onClick={handleClick}>
+      {isReviled && cardLabel}
     </div>
   );
 }
